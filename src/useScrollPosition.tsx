5754f4cdd3aafbb0ef0f006f62354540597f0e32
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-06-08 01:18:27 +0200

    Add useScrollPosition hook

---
+++ src/useScrollPosition.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+const useScrollPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState<number | null>(null);
+  useEffect(() => {
+    const handleScroll = () => setScrollPosition(window.scrollY);
+    document.addEventListener('scroll', handleScroll);
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
+  return scrollPosition;
+};
+export default useScrollPosition;
