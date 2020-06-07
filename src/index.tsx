54a92ba88d70c21a938c1c0c3f8b2fd091c5b8f2
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-06-08 01:34:37 +0200

    Fix exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 export { default as usePageWidth } from './usePageWidth';
-export { default as useString } from './useString';
 export { default as useHelloRomanJoke } from './useHelloRomanJoke';
 export { default as useUnsplash } from './useUnsplash';
 export { default as useClickOutside } from './useClickOutside';
