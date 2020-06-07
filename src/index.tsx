c809adbc081650a087564606a6b9d14e2f6cd410
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-06-08 01:36:09 +0200

    Update exports

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ export { default as usePageWidth } from './usePageWidth';
 export { default as useHelloRomanJoke } from './useHelloRomanJoke';
 export { default as useUnsplash } from './useUnsplash';
 export { default as useClickOutside } from './useClickOutside';
+export { default as useGeoPosition } from './useGeoPosition';
+export { default as useIdle } from './useIdle';
+export { default as useScrollPosition } from './useScrollPosition';
