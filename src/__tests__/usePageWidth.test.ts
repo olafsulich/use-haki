403c01f0de0da3814d807ee81a49f888ce60ea81
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-06-21 18:39:14 +0200

    Add tests to usePageWidth hook

---
+++ src/__tests__/usePageWidth.test.ts
@@ -0,0 +1,32 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import usePageWidth from '../usePageWidth';
+
+describe('test usePageWidth hook', () => {
+  const getHook = () => renderHook(() => usePageWidth());
+
+  const triggerResize = (value: number) => {
+    (window.innerWidth as number) = value;
+    window.dispatchEvent(new Event('resize'));
+  };
+
+  it('should return numeric value', () => {
+    const { result } = getHook();
+    expect(typeof result.current).toBe('number');
+  });
+
+  it('should re-render after width change', () => {
+    const { result } = getHook();
+
+    act(() => {
+      triggerResize(550);
+    });
+
+    expect(result.current).toBe(550);
+
+    act(() => {
+      triggerResize(920);
+    });
+
+    expect(result.current).toBe(920);
+  });
+});
