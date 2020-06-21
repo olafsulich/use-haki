0f5d1226d91d31d59e962d71d29c104ee3b2e080
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-06-21 14:30:37 +0200

    Move ordinals to separate file

--- src/usePluralRules.ts
+++ src/usePluralRules.ts
@@ -1,14 +1,7 @@
+import { ordinals } from './helpers/ordinals';
+
 const usePluralRules = (num: number) => {
   const pr = new Intl.PluralRules('en-US', { type: 'ordinal' });
-
-  const ordinals = {
-    one: 'st',
-    two: 'nd',
-    few: 'rd',
-    many: 'th',
-    zer: 'th',
-    other: 'th',
-  };
 
   return `${num}${ordinals[pr.select(num)]}`;
 };
